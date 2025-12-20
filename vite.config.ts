631daff970ecdbc4a4c487164133044d7a7a870e
cesarferreira <cesar.manuel.ferreira@gmail.com>
2025-12-20 00:25:10 +0000

    renamed base

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/json/',
+  base: '/parsy/',
   build: {
     rollupOptions: {
       input: {
